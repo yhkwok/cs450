876e836849d22b3e7baf5efaeae1582f50662a81
yhkwok <kwo13001@byui.edu>
2015-05-16 13:03:45 -0600

    created the frame for decision tree

---
+++ src/expshell/DecisionTree.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package expshell;
+
+import weka.classifiers.Classifier;
+import weka.core.Instance;
+import weka.core.Instances;
+
+/**
+ *
+ * @author YH Jonathan Kwok
+ */
+public class DecisionTree extends Classifier{
+    private Instances data;
+    
+    @Override
+    public void buildClassifier(Instances data) throws Exception {
+        this.data = data;
+    }
+    
+    @Override
+    public double classifyInstance(Instance i) {
+        
+    }
+    
+}
