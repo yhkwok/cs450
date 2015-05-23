4192449d4d3c9a99bb0a509809a7f0e336a4ab53
yhkwok <kwo13001@byui.edu>
2015-05-23 14:51:33 -0600

    the Neural class~~

---
+++ src/expshell/Neural.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package expshell;
+
+import java.util.ArrayList;
+import java.util.List;
+import weka.core.Debug.Random;
+
+/**
+ *
+ * @author YH Jonathan Kwok
+ */
+public class Neural {
+    
+    List<Double> values = new ArrayList<Double>();
+    List<Double> weights = new ArrayList<Double>();
+    double randMin = 0.1;
+    double randMax = 5.0;
+    
+    public Neural(List<Double> vals){
+        values = vals;
+        //add the bias input at the end
+        values.add(-1.0);
+        //set the weights according to the size of the values we have
+        setWeights();
+    }
+    
+    public void setWeights(){
+        for(Double temp : values)
+        {
+            Random r = new Random();
+            double randomValue = randMin + (randMax - randMin) * r.nextDouble();
+            weights.add(randomValue);
+        }
+    }   
+    
+    public int cal(){
+        double sum = 0;
+        for(int i = 0; i < values.size(); i++)
+        {
+            sum += values.get(i) * weights.get(i);
+        }
+        if (sum > 0)
+            return 1;
+        return 0;
+    }
+}
